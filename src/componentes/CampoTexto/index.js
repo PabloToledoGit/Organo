74758834396f420de0eef01cffdaaa9344c63ca6
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-08-16 16:58:48 -0300

    estudando react

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return(
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='digite o seu nome' />
+        </div>
+
+    )
+    
+}
+
+export default CampoTexto;
