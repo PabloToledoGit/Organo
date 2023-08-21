1fd6ca5b4a1edaa5d4a9182af206a5635e8ad41e
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-08-21 16:45:46 -0300

    ajustando

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,17 @@
 import './CampoTexto.css'
 
+let valor = 'Guilherme Silveira'
+
+const aoDigitado = (evento) =>{
+    valor = evento.target.value
+    console.log(valor)
+}
+
 const CampoTexto = (props) => {
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
 
     )
