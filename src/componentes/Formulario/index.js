bf85b8dc570bfdd98423ad579e2ec55f7720d42d
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-08-21 15:45:16 -0300

    adicionando verificação de dados preenchidos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -13,18 +14,24 @@ const Formulario = () => {
     'Ux e Design',
     'Mobile',
     'Inovação e Gestão',
-
   ]
 
+  const aoSalvar = (evento) =>{
+    console.log('foi submetido')
+    evento.preventDefault()
+  }
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h1>Preencha os dados para criar o card do colaborador</h1>
-        <CampoTexto label="nome" placeholder="digite seu nome" />
-        <CampoTexto label="cargo" placeholder="digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="nome" placeholder="digite seu nome" />
+        <CampoTexto obrigatorio={true}  label="cargo" placeholder="digite seu cargo" />
         <CampoTexto label="imagem" placeholder="digite o link da sua imagem" />
         <ListaSuspensa label="time" itens={times} />
+        <Botao texto='Criar Card'>
+            Criar Card
+        </Botao>
       </form>
     </section>
   );
